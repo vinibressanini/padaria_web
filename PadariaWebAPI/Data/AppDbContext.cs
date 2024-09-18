920b0c1d9b634500c9add97d1e30b2a974689be9
vinibressanini <vinimarcus41@gmail.com>
2024-09-18 10:16:46 -0300

    feat: implemented points api endpoint

---
+++ PadariaWebAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PadariaWebAPI.Models;
+
+namespace PadariaWebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LoyalCustomer> Customer { get; set; }
+
+    }
+}
---
+++ PadariaWebAPI/Models/LoyalCustomer.cs
@@ -0,0 +1,16 @@
+﻿namespace PadariaWebAPI.Models
+{
+    public class LoyalCustomer
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public double Points { get; set; } = 0.0;
+
+
+    }
+
+
+
+}
